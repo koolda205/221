3135bdade8e0cef89a0a4c9d91f94900cb3f3672
Dmitrii Kulakov <koolda@yandex.ru>
2023-05-19 15:40:50 +0300

    add Car Class and add Car.class inti AppConfig

---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -0,0 +1,54 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private int id;
+
+   @Column(name = "model")
+   private String model;
+
+   @Column(name = "series")
+   private int series;
+
+    public Car() {}
+
+   public Car(String model, int series) {
+      this.model = model;
+      this.series = series;
+   }
+
+   public void setId(int id) {
+      this.id = id;
+   }
+
+   public int getId() {
+      return id;
+   }
+
+   public String getModel() {
+      return model;
+   }
+
+   public void setSeries(int series) {
+      this.series = series;
+   }
+
+   public int getSeries() {
+      return series;
+   }
+
+   @Override
+   public String toString() {
+      return "Car{" +
+              "id=" + id +
+              ", model='" + model + '\'' +
+              ", series=" + series +
+              '}';
+   }
+}
